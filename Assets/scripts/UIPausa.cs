cdd45e503a22eafe0df2e20fc2f560d26850c109
punkryuu <rodrigodudi2005@gmail.com>
2025-09-20 10:05:01 +0200

    Revert "Merge pull request #16 from punkryuu/menuPausa"
    This reverts commit 01d4425f2fcec772eb282523e92bcf573209ed31, reversing
    changes made to 735b436580604602de2254d3a1ba4d692d641f22.

--- Assets/scripts/UIPausa.cs
+++ Assets/scripts/UIPausa.cs
@@ -4,55 +4,15 @@ using UnityEngine;
 
 public class UIPausa : MonoBehaviour
 {
-    public static bool GameIsPaused;
-    [SerializeField] private GameObject pauseMenuUI;
-    [SerializeField] private GameObject optionsMenu;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        GameIsPaused = false;
+        
     }
+
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            // Si el menú de opciones está activo, no hagas nada
-            if (optionsMenu.activeSelf)
-            {
-                return;
-            }
-
-            // Si el juego está pausado y el menú de opciones no está activo, reanuda el juego
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            // Si el juego no está pausado y el menú de opciones no está activo, pausa el juego
-            else
-            {
-                Pause();
-            }
-        }
-    }
-    private void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
-
-    }
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-
-    }
-    public void QuitGame()
-    {
-        Debug.Log("Quit");
-        Application.Quit();
+        
     }
 }
