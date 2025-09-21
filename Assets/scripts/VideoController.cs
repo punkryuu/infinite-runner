9c41c70ee00ba360bf3b9510f9c0590e1906298b
NathanPorro <nathanp0rr03@gmail.com>
2025-09-21 11:34:22 +0200

    QueTerrible

---
+++ Assets/scripts/VideoController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer reproductorCinematica1;
+    public VideoPlayer reproductorCinematica2;
+    public RawImage cargaCinematica;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cargaCinematica.gameObject.SetActive(false);
+        reproductorCinematica1.targetTexture = (RenderTexture)cargaCinematica.texture;
+        reproductorCinematica2.targetTexture = (RenderTexture)cargaCinematica.texture;
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CasosMuerte(int contadorMuerte)
+    {
+
+        switch (contadorMuerte)
+        {
+            case 1:
+                cargaCinematica.gameObject.SetActive(true);
+                reproductorCinematica1.Play();
+                reproductorCinematica1.loopPointReached += TerminaCinematica;
+                break;
+            case 2:
+                cargaCinematica.gameObject.SetActive(true);
+                reproductorCinematica2.Play();
+                reproductorCinematica2.loopPointReached += TerminaCinematica;
+                break;
+        }
+    }
+
+    public void BotonCinematica1()
+    {
+       
+         if (GameManager.instancia.contadorMuerte >=1 && VideoManager.instancia.cinematica1Vista)
+        {
+            MostrarCinematica(reproductorCinematica1);
+        }
+    }
+    public void BotonCinematica2()
+    {
+        if (GameManager.instancia.contadorMuerte >= 2 && VideoManager.instancia.cinematica2Vista)
+        {
+            MostrarCinematica(reproductorCinematica2);
+        }
+       
+    }
+
+
+    public void MostrarCinematica(VideoPlayer vid)
+    {
+        cargaCinematica.gameObject.SetActive(true);
+        vid.Play();
+        vid.loopPointReached += TerminaCinematica;
+    }
+    public void TerminaCinematica(VideoPlayer vid)
+    {
+        cargaCinematica.gameObject.SetActive(false);
+        SceneManager.LoadScene("PantallaDeInicio");
+        GameManager.instancia.escenaCargada = false;
+    }
+}
--- Assets/scripts/VideoManager.cs
+++ Assets/scripts/VideoManager.cs
@@ -6,40 +6,43 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 using UnityEngine.UI;
 
+
 public class VideoManager : MonoBehaviour
 {
-    public VideoPlayer reproductor;
-    public RawImage cargaCinematica1;
+    public static VideoManager instancia;
    
+    public VideoManager videoManager;
+    public bool cinematica1Vista = false;
+    public bool cinematica2Vista = false;
+    int numeroCinematica;
+
+    private void Awake()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    
+}
     private void Start()
     {
-        cargaCinematica1.gameObject.SetActive(false);
+        
         if (GameManager.instancia == null)
         {
             GameManager.instancia = FindObjectOfType<GameManager>();
         }
-    
 }
-    public void BotonCinematica1()
+      
+    public void DesbloquearCinematica()
     {
-        if(GameManager.instancia.contadorMuerte >= 1)
-        {
-            cargaCinematica1.gameObject.SetActive(true);
-            reproductor.Play();
-            reproductor.loopPointReached += TerminaCinematica;
-        }
+        if (GameManager.instancia.contadorMuerte >= 1) cinematica1Vista = true;
+        if (GameManager.instancia.contadorMuerte >= 2) cinematica2Vista = true;
     }
-    void TerminaCinematica(VideoPlayer vid)
-    {
-        cargaCinematica1.gameObject.SetActive(false);
-        SceneManager.LoadScene("PantallaDeInicio");
-    }
-    void BotonCreditos()
-    {
-        SceneManager.LoadScene("SampleScene");
-    }
-    void BotonCinematicas()
-    {
-        SceneManager.LoadScene("SampleScene");
-    }
+
+
 }
